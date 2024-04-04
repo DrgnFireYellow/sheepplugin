a58efaf92506880badb5dd44ad15083af4287124
DrgnFireYellow <DrgnFireYellow@users.noreply.github.com>
2024-04-04 11:40:28 -0400

    Make it so that the sheep have random colors

--- src/main/java/com/drgnfireyellow/sheepplugin/commands/sheepCommand.java
+++ src/main/java/com/drgnfireyellow/sheepplugin/commands/sheepCommand.java
@@ -1,19 +1,24 @@
 package com.drgnfireyellow.sheepplugin.commands;
 
+import java.util.Random;
+
 import org.bukkit.command.Command;
 import org.bukkit.command.CommandExecutor;
 import org.bukkit.command.CommandSender;
 import org.bukkit.entity.*;
 import org.bukkit.Location;
 import org.bukkit.World;
+import org.bukkit.DyeColor;
 
 public class sheepCommand implements CommandExecutor {
     @Override
     public boolean onCommand(CommandSender sender, Command command, String label, String[] args) {
         if (sender instanceof Player) {
+            DyeColor[] colors = {DyeColor.WHITE, DyeColor.LIGHT_GRAY, DyeColor.GRAY, DyeColor.BLACK, DyeColor.BROWN, DyeColor.RED, DyeColor.ORANGE, DyeColor.YELLOW, DyeColor.LIME, DyeColor.GREEN, DyeColor.CYAN, DyeColor.LIGHT_BLUE, DyeColor.BLUE, DyeColor.PURPLE, DyeColor.MAGENTA, DyeColor.PINK};
             sender.sendMessage("Baaaaa!");
             Player player = (Player) sender;
-            player.getWorld().spawnEntity(player.getLocation(), EntityType.SHEEP);
+            Sheep sheep = (Sheep) player.getWorld().spawnEntity(player.getLocation(), EntityType.SHEEP);
+            sheep.setColor(colors[new Random().nextInt(colors.length)]);
         }
         return false;
     }
